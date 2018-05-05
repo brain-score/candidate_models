8a8fa7a68fe25416a22389c28513414b3cd545ef
Martin Schrimpf <m4rtinsch@gmail.com>
2018-05-05 17:27:35 -0400

    rename load_stimuli -> load_stimulus_set

--- neurality/assemblies.py
+++ neurality/assemblies.py
@@ -1,13 +1,15 @@
 import mkgu
 
 from neurality.storage import cache
+from mkgu.benchmarks import load as load_benchmark
 
 
 @cache()
-def load_neural_benchmark(data_name, metric_name):
-    return mkgu.metrics.benchmarks.load(data_name=data_name, metric_name=metric_name)
+def load_neural_benchmark(assembly_name, metric_name):
+    return load_benchmark(data_name=assembly_name, metric_name=metric_name)
 
 
-def load_stimuli(name):
-    stimuli = mkgu.get_stimuli(name).load()
-    return stimuli
+def load_stimulus_set(assembly_name):
+    assembly = mkgu.get_assembly(assembly_name)
+    stimulus_set = assembly.attrs['stimulus_set']
+    return stimulus_set
