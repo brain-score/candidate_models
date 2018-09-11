1f7968015863a24df2ba00a893a658ac4a6a539a
Martin Schrimpf <m4rtinsch@gmail.com>
2018-09-11 05:43:51 -0400

    fix using predefined version

--- tests/models/implementations/test_tensorflow_slim.py
+++ tests/models/implementations/test_tensorflow_slim.py
@@ -2,9 +2,10 @@ import os
 
 import numpy as np
 import pytest
-from brainscore.assemblies import NeuroidAssembly
-from candidate_models.models.implementations.tensorflow_slim import TensorflowSlimModel
 from tests.models import mock_imagenet
+
+from brainscore.assemblies import NeuroidAssembly
+from candidate_models.models.implementations.tensorflow_slim import TensorflowSlimModel, TensorflowSlimPredefinedModel
 
 
 class TestLoadPreprocessImage:
@@ -20,7 +21,7 @@ class TestLoadPreprocessImage:
 
 class TestModels:
     def test_single_image_resnet101v2(self):
-        model = TensorflowSlimModel('resnet-101_v2', batch_size=1, image_size=224)
+        model = TensorflowSlimPredefinedModel('resnet-101_v2', batch_size=1, image_size=224)
         stimuli_paths = [os.path.join(os.path.dirname(__file__), 'rgb.jpg')]
         layer = "resnet_v2_101/block4/unit_3/bottleneck_v2"
         activations = model.get_activations(stimuli_paths, layers=[layer], pca_components=None)
@@ -28,7 +29,7 @@ class TestModels:
         assert len(activations['stimulus_path']) == 1
 
     def test_single_image_pad_resnet101v2(self):
-        model = TensorflowSlimModel('resnet-101_v2', batch_size=2, image_size=224)
+        model = TensorflowSlimPredefinedModel('resnet-101_v2', batch_size=2, image_size=224)
         stimuli_paths = [os.path.join(os.path.dirname(__file__), 'rgb.jpg')]
         layer = "resnet_v2_101/block4/unit_3/bottleneck_v2"
         activations = model.get_activations(stimuli_paths, layers=[layer], pca_components=None)
@@ -36,7 +37,7 @@ class TestModels:
         assert len(activations['stimulus_path']) == 1
 
     def test_two_batches_single_image_resnet101v2(self):
-        model = TensorflowSlimModel('resnet-101_v2', batch_size=1, image_size=224)
+        model = TensorflowSlimPredefinedModel('resnet-101_v2', batch_size=1, image_size=224)
         stimuli_paths = [os.path.join(os.path.dirname(__file__), 'rgb.jpg')] * 2
         layer = "resnet_v2_101/block4/unit_3/bottleneck_v2"
         activations = model.get_activations(stimuli_paths, layers=[layer], pca_components=None)
@@ -44,7 +45,7 @@ class TestModels:
         assert len(activations['stimulus_path']) == 2
 
     def test_two_batches_two_images_resnet101v2(self):
-        model = TensorflowSlimModel('resnet-101_v2', batch_size=2, image_size=224)
+        model = TensorflowSlimPredefinedModel('resnet-101_v2', batch_size=2, image_size=224)
         stimuli_paths = [os.path.join(os.path.dirname(__file__), 'rgb.jpg')] * 4
         layer = "resnet_v2_101/block4/unit_3/bottleneck_v2"
         activations = model.get_activations(stimuli_paths, layers=[layer], pca_components=None)
@@ -52,7 +53,7 @@ class TestModels:
         assert len(activations['stimulus_path']) == 4
 
     def test_pca_resnet101v2(self):
-        model = TensorflowSlimModel('resnet-101_v2', batch_size=1, image_size=224)
+        model = TensorflowSlimPredefinedModel('resnet-101_v2', batch_size=1, image_size=224)
         mock_imagenet(model)
         stimuli_paths = [os.path.join(os.path.dirname(__file__), 'rgb.jpg')]
         layer = "resnet_v2_101/block4/unit_3/bottleneck_v2"
@@ -64,8 +65,8 @@ class TestModels:
 
 class TestGraph:
     def test_resnet101(self):
-        model = TensorflowSlimModel('resnet-101_v2')
+        model = TensorflowSlimPredefinedModel('resnet-101_v2')
         graph = model.graph()
         assert 146 == len(graph.nodes)
         assert 101 == len([node_name for node_name, node in graph.nodes.items()
-                         if 'conv' in node['object'].name.lower() or 'logits' in node['object'].name.lower()])
+                           if 'conv' in node['object'].name.lower() or 'logits' in node['object'].name.lower()])
