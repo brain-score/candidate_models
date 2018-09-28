ee3a3f273b9f356d55229a277637515f7a25d4df
Martin Schrimpf <m4rtinsch@gmail.com>
2018-09-28 15:02:09 -0400

    clarify behavioral differences output

--- candidate_models/analyze/individual_behavior.py
+++ candidate_models/analyze/individual_behavior.py
@@ -3,11 +3,7 @@ import os
 import numpy as np
 
 
-def compute_behavioral_differences():
-    diff('pnasnet_large')
-
-
-def diff(model):
+def compute_behavioral_differences(model='pnasnet_large'):
     data = np.load(os.path.join(os.path.dirname(__file__), '{}.npy'.format(model)))
     model_responses, human_responses = data[:, 0], data[:, 1]
     pairs = zip(human_responses, model_responses)
@@ -17,9 +13,9 @@ def diff(model):
     human_better = sum(diffs > 1)
     model_better = sum(diffs < -1)
     print(f"Total number of images: {len(model_responses)} {model} responses and {len(human_responses)} human responses")
-    print(f"Roughly equal: {equal} ({equal / num_images * 100:.2f}%)")
-    print(f"Human better: {human_better} ({human_better / num_images * 100:.2f}%)")
-    print(f"Model better: {model_better} ({model_better / num_images * 100:.2f}%)")
+    print(f"Roughly equal (-1<=d<=1): {equal} ({equal / num_images * 100:.2f}%)")
+    print(f"Human better (d>1): {human_better} ({human_better / num_images * 100:.2f}%)")
+    print(f"Model better (d<-1): {model_better} ({model_better / num_images * 100:.2f}%)")
     print(f"Sanity check sum: {equal + human_better + model_better}")
 
 
