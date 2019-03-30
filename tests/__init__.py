71e7acb5bcca2daa8b42186008f7def309528d74
Martin Schrimpf <m4rtinsch@gmail.com>
2019-03-29 23:35:03 -0400

    set noise logger levels to warning

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,5 @@ import logging
 import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)-15s %(levelname)s:%(name)s:%(message)s')
+for disable_logger in ['s3transfer', 'botocore', 'boto3', 'urllib3', 'peewee', 'PIL']:
+    logging.getLogger(disable_logger).setLevel(logging.WARNING)
