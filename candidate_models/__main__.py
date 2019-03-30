241755d3162ea665965da02c7c6e1f8ab98eac87
Martin Schrimpf <m4rtinsch@gmail.com>
2019-03-29 23:34:16 -0400

    add main function to retrieve activations

--- candidate_models/__main__.py
+++ candidate_models/__main__.py
@@ -2,38 +2,46 @@ import logging
 import sys
 
 import argparse
+import fire
 
-from candidate_models import score_layers, score_model
+from candidate_models import score_layers as score_layers_function, score_model as score_model_function, get_activations
 from candidate_models.model_commitments import model_layers_pool, brain_translated_pool
 
 logger = logging.getLogger(__name__)
 
-
-def main():
-    score_functions = {'score_layers': score_layers, 'score': score_model}
-    model_pools = {'score_layers': model_layers_pool, 'score': brain_translated_pool}
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, required=True)
-    parser.add_argument('--scoring_type', type=str, choices=score_functions.keys(), default='score')
-    parser.add_argument('--benchmark', type=str, required=True)
-    parser.add_argument('--log_level', type=str, default='INFO')
-    args = parser.parse_args()
-    logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(args.log_level))
-    for disable_logger in ['s3transfer', 'botocore', 'boto3', 'urllib3']:
-        logging.getLogger(disable_logger).setLevel(logging.WARNING)
-    logging.getLogger("peewee").setLevel(logging.WARNING)
-    logger.info("Running with args %s", vars(args))
-
-    logger.info('Scoring model')
-    score_function = score_functions[args.scoring_type]
-    score_pool = model_pools[args.scoring_type]
-    model = score_pool[args.model]
-    if not isinstance(model, dict):
-        score = score_function(args.model, model=model, benchmark_identifier=args.benchmark)
-    else:
-        score = score_function(args.model, **model, benchmark_identifier=args.benchmark)
-    print(score)
+parser = argparse.ArgumentParser()
+parser.add_argument('--log_level', type=str, default='INFO')
+args, _ = parser.parse_known_args()
+logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(args.log_level))
+for disable_logger in ['s3transfer', 'botocore', 'boto3', 'urllib3', 'peewee', 'PIL']:
+    logging.getLogger(disable_logger).setLevel(logging.WARNING)
 
 
-main()
+def activations(model, assembly, layers=None):
+    if isinstance(layers, str):
+        layers = [layers]
+    model_layers = model_layers_pool[model]
+    model, default_layers = model_layers['model'], model_layers['layers']
+    layers = layers or default_layers
+    result = get_activations(model, layers=layers, assembly_identifier=assembly)
+    print(result)
+
+
+def score_model(model, benchmark):
+    model = brain_translated_pool[model]
+    result = score_model_function(model_identifier=model, benchmark_identifier=benchmark, model=model)
+    print(result)
+
+
+def score_layers(model, benchmark, layers=None):
+    if isinstance(layers, str):
+        layers = [layers]
+    model_layers = model_layers_pool[model]
+    model, default_layers = model_layers['model'], model_layers['layers']
+    layers = layers or default_layers
+    result = score_layers_function(model_identifier=model, benchmark_identifier=benchmark, model=model,
+                                   layers=layers)
+    print(result)
+
+
+fire.Fire()
