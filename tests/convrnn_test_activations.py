bf452791fb607d8876dfd3764b05591a66e70aa0
Aran Nayebi <anayebi@stanford.edu>
2019-04-04 14:30:18 -0700

    Added convrnn test init

--- tests/convrnn_test_activations.py
+++ tests/convrnn_test_activations.py
@@ -1,7 +1,7 @@
 import numpy as np
 from candidate_models.base_models import base_model_pool
 from candidate_models.model_commitments import model_layers
-stimuli_path = 'image.jpg'
+stimuli_path = '/home/anayebi/candidate_models/examples/image.jpg'
 
 model = base_model_pool['convrnn_224']
 layers = model_layers['convrnn_224']
---
+++ tests/convrnn_test_imagenet.py
@@ -0,0 +1,20 @@
+import pytest
+from brainscore.benchmarks.imagenet import Imagenet2012
+
+from candidate_models import brain_translated_pool
+
+
+class TestImagenet:
+    @pytest.mark.parametrize(['model', 'expected_top1'], [
+        ('resnet-101_v2', .770),
+    ])
+    def test_top1(self, model, expected_top1):
+        model = brain_translated_pool[model]
+        benchmark = Imagenet2012()
+        score = benchmark(model)
+        accuracy = score.sel(aggregation='center')
+        assert accuracy == expected_top1
+
+if __name__ == '__main__':
+    ff_test = TestImagenet()
+    ff_test.test_top1(model='resnet-101_v2', expected_top1=0.770)
