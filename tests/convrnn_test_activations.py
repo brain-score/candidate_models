4cb4eddd6160eb884121b7bdae88f08369083c6b
Aran Nayebi <anayebi@stanford.edu>
2019-04-19 16:18:53 -0700

    Just keeping imnet test

--- tests/convrnn_test_activations.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from candidate_models.base_models import base_model_pool
-from candidate_models.model_commitments import model_layers
-stimuli_path = '/home/anayebi/candidate_models/examples/image.jpg'
-
-model = base_model_pool['convrnn_224']
-layers = model_layers['convrnn_224']
-layers = [layers[-1]]
-activations = model(stimuli=[stimuli_path], layers=layers)
