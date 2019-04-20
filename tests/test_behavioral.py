180afe33328c2a9a4805212f0c5d32f0af314384
Martin Schrimpf <m4rtinsch@gmail.com>
2019-04-19 21:23:41 -0400

    add test for alexnet on behavior

---
+++ tests/test_behavioral.py
@@ -0,0 +1,9 @@
+from pytest import approx
+
+from candidate_models import brain_translated_pool, score_model
+
+
+def test_alexnet():
+    model = brain_translated_pool['alexnet']
+    score = score_model(model_identifier='alexnet', model=model, benchmark_identifier='dicarlo.Rajalingham2018-i2n')
+    assert score.raw.sel(aggregation='center') == approx(.253, abs=.005)
