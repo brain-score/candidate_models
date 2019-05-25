cb31d166e23fe7648099808eb82f55605c6d73bd
Martin Schrimpf <m4rtinsch@gmail.com>
2019-05-25 13:46:36 -0400

    move cornet_r2 to its own file

--- candidate_models/base_models/cornet/__init__.py
+++ candidate_models/base_models/cornet/__init__.py
@@ -4,10 +4,8 @@ import os
 from collections import defaultdict
 
 import functools
-import math
 import re
 import torch
-from torch import nn
 from torch.nn import Module
 
 from brainio_base.assemblies import merge_data_arrays, NeuroidAssembly, walk_coords
@@ -74,6 +72,7 @@ class TemporalPytorchWrapper(PytorchWrapper):
 def cornet(identifier):
     cornet_type = re.match('CORnet-(.*)', identifier).group(1)
     if cornet_type.lower() == 'r2':
+        from .cornet_r2 import CORNetR2
         model_ctr = CORNetR2
     elif cornet_type.lower() == 's10':
         from .cornet_s_10 import CORnet_S as CORnet_S10
@@ -125,121 +124,3 @@ def cornet(identifier):
     wrapper = TemporalPytorchWrapper(identifier=identifier, model=model, preprocessing=preprocessing)
     wrapper.image_size = 224
     return wrapper
-
-
-class CORBlock_Rec2(nn.Module):
-    scale = 6
-
-    def __init__(self, in_channels, out_channels, ntimes=1, stride=1, h=None, name=None):
-        super(CORBlock_Rec2, self).__init__()
-
-        self.name = name
-        self.ntimes = ntimes
-        self.stride = stride
-
-        self.conv_first = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-        self.shortcut = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=stride, bias=False)
-        self.shortcut_bn = nn.BatchNorm2d(out_channels)
-
-        # self.conv1 = nn.Conv2d(out_channels, out_channels * self.scale, kernel_size=1, bias=False)
-        # self.relu1 = nn.ReLU(inplace=True)
-
-        self.conv2 = nn.Conv2d(out_channels, out_channels * self.scale,
-                               kernel_size=3, stride=stride, padding=1, bias=False)
-        self.relu2 = nn.ReLU(inplace=True)
-
-        self.conv3 = nn.Conv2d(out_channels * self.scale, out_channels, kernel_size=1, bias=False)
-        self.relu3 = nn.ReLU(inplace=True)
-
-        for n in range(ntimes):
-            setattr(self, f'bn1_{n}', nn.BatchNorm2d(out_channels * self.scale))
-            setattr(self, f'bn2_{n}', nn.BatchNorm2d(out_channels * self.scale))
-            setattr(self, f'bn3_{n}', nn.BatchNorm2d(out_channels))
-
-    def forward(self, inp):
-        inp = self.conv_first(inp)
-        # state = torch.zeros_like(inp)
-
-        for n in range(self.ntimes):
-            # x = inp + state # torch.stack([inp, state], dim=1)
-            if n == 0:
-                # x = self.conv_first(inp)
-                x = inp
-                residual = self.shortcut_bn(self.shortcut(inp))
-                inp = residual
-                # state = torch.zeros_like(inp)
-            else:
-                residual = inp + state
-                x = residual
-
-            # x = self.conv1(x)
-            # x = getattr(self, f'bn1_{n}')(x)
-            # x = self.relu1(x)
-
-            if n == 0 and self.stride == 2:
-                self.conv2.stride = (2, 2)
-            else:
-                self.conv2.stride = (1, 1)
-            x = self.conv2(x)
-            x = getattr(self, f'bn2_{n}')(x)
-            x = self.relu2(x)
-
-            x = self.conv3(x)
-            x = getattr(self, f'bn3_{n}')(x)
-
-            x += residual
-            state = self.relu3(x)
-
-        return x
-
-
-class CORNetR2(nn.Module):
-    def __init__(self, ntimes=(5, 5, 5), num_classes=1000):
-        super(CORNetR2, self).__init__()
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        # self.conv2 = nn.Conv2d(64, , kernel_size=3, stride=2, padding=1,
-        #                        bias=False)
-        # self.bn2 = nn.BatchNorm2d(128)
-
-        self.block2 = CORBlock_Rec2(64, 128, ntimes=ntimes[0], stride=2, h=28, name='b0')
-        self.block3 = CORBlock_Rec2(128, 256, ntimes=ntimes[1], stride=2, h=14, name='b1')
-        self.block4 = CORBlock_Rec2(256, 512, ntimes=ntimes[2], stride=2, h=7, name='b2')
-
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(512, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                # nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        # x = self.conv2(x)
-        # x = self.bn2(x)
-        # x = self.relu(x)
-
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
