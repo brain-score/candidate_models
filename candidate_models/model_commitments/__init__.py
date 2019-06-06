c07917811cd566f42a16775ac010a780a64f62dc
Martin Schrimpf <m4rtinsch@gmail.com>
2019-06-06 10:55:04 -0400

    set `brain_translated_pool` dict directly without register function

--- candidate_models/model_commitments/__init__.py
+++ candidate_models/model_commitments/__init__.py
@@ -6,14 +6,9 @@ from candidate_models.utils import UniqueKeyDict
 
 brain_translated_pool = UniqueKeyDict()
 
-
-def register_brain_model(identifier, model):
-    brain_translated_pool[identifier] = model
-
-
 for identifier, model in ml_brain_pool.items():
-    register_brain_model(identifier, model)
-register_brain_model('resnet-101_v2-layer_timesteps', LazyLoad(resnet_layer_timesteps))
+    brain_translated_pool[identifier] = model
+brain_translated_pool['resnet-101_v2-layer_timesteps'] = LazyLoad(resnet_layer_timesteps)
 
 for identifier, model in cornet_brain_pool.items():
-    register_brain_model(identifier, model)
+    brain_translated_pool[identifier] = model
