b21e14de98be9800414ddfac9236e2208d7326a6
Martin Schrimpf <m4rtinsch@gmail.com>
2019-07-13 13:19:38 -0400

    update layer-commitments test: remove CORnet, circumvent lazy commit

--- tests/test_model_commitments.py
+++ tests/test_model_commitments.py
@@ -8,8 +8,9 @@ from candidate_models import brain_translated_pool
 class TestBestLayers:
     @pytest.mark.parametrize(['model_identifier', 'expected'], [
         ('alexnet', {'V1': 'features.5', 'V2': 'features.5', 'V4': 'features.5', 'IT': 'features.12'}),
-        ('CORnet-S', {'V1': 'V1.output-t0', 'V2': 'V2.output-t1', 'V4': 'V4.output-t0', 'IT': 'IT.output-t0'}),
     ])
     def test(self, model_identifier, expected):
         model = brain_translated_pool[model_identifier]
+        for region in expected:  # need to first initialize mapping (only run lazily)
+            model.start_recording(region, [(70, 170)])
         assert model.layer_model.region_layer_map == expected
