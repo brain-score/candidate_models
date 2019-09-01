d135ed4c8e1bcca15f0f9d49b322d8647636dd71
Martin Schrimpf <m4rtinsch@gmail.com>
2019-09-01 11:03:13 -0400

    use git install prefix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
     "cornet @ git+https://github.com/dicarlolab/CORnet",
     "bagnets @ git+https://github.com/mschrimpf/bag-of-local-features-models.git",
     "texture_vs_shape @ git+https://github.com/mschrimpf/texture-vs-shape.git",
-    "git+https://github.com/mschrimpf/FixRes.git",
+    "Fixing-the-train-test-resolution-discrepancy-scripts @ git+https://github.com/mschrimpf/FixRes.git",
 ]
 
 setup(
