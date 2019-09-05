3ef6dbd1bb91ff390c43d1c2463e6a89765d1537
Martin Schrimpf <m4rtinsch@gmail.com>
2019-09-05 16:22:55 -0400

    mark as memory_intense

--- tests/base_models/test_cornet.py
+++ tests/base_models/test_cornet.py
@@ -1,8 +1,10 @@
+import pytest
 from pathlib import Path
 
 from candidate_models.base_models.cornet import cornet
 
 
+@pytest.mark.memory_intense
 def test_no_time_separation():
     model = cornet('CORnet-S', separate_time=False)
     stimulus_path = Path(__file__).parent / 'rgb.jpg'
