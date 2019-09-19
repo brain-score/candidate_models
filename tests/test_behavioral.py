28a1d43cd2237a7ff836b91a36468a34ca8b67d4
franzigeiger <franzigeiger94@gmail.com>
2019-09-19 10:19:32 -0400

    One missing method header change

--- tests/test_behavioral.py
+++ tests/test_behavioral.py
@@ -8,5 +8,5 @@ from candidate_models import brain_translated_pool, score_model
 @pytest.mark.private_access
 def test_alexnet():
     model = brain_translated_pool['alexnet']
-    score = score_model(model=model, benchmark_identifier='dicarlo.Rajalingham2018-i2n')
+    score = score_model(model_identifier='alexnet', model=model, benchmark_identifier='dicarlo.Rajalingham2018-i2n')
     assert score.raw.sel(aggregation='center') == approx(.253, abs=.005)
