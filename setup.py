be7ca244b2db460ba7afd8ad91944b6081814e80
franzigeiger <franzigeiger94@gmail.com>
2020-02-27 18:22:30 -0500

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -17,8 +17,9 @@ requirements = [
     "gitpython",
     "torch",
     "keras",
+    "pandas==0.25.3"
     "tensorflow==1.15",
-
+    "Pillow",
     "cornet @ git+https://github.com/dicarlolab/CORnet",
     "bagnets @ git+https://github.com/mschrimpf/bag-of-local-features-models.git",
     "texture_vs_shape @ git+https://github.com/mschrimpf/texture-vs-shape.git",
