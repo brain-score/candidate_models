ea2713a3072e720606c3a76993765619e6c0fbd2
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2021-01-30 07:37:41 -0500

    use generic netcdf4 instead of specifying version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    "netcdf4==1.5.2",
+    "netcdf4",
     "brain-score @ git+https://github.com/brain-score/brain-score",
     "model-tools @ git+https://github.com/brain-score/model-tools",
     "numpy",
