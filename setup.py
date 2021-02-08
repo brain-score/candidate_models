3b63dc9467f61a4b177c740b297e2a046300bcd5
Martin Schrimpf <m4rtinsch@gmail.com>
2021-02-08 10:09:21 -0500

    upgrade to python 3.7
    discontinue python 3.6, together with https://github.com/brain-score/model-tools/pull/48

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
 )
