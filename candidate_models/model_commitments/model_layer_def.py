7228014188ab8033ee35e372d0e6b1969158d8cf
Joel Dapello <dapello@openmind7.ib.cluster>
2021-03-02 12:14:28 -0500

    updated CM with a number of pytorch models and adversarially trained models

--- candidate_models/model_commitments/model_layer_def.py
+++ candidate_models/model_commitments/model_layer_def.py
@@ -2,6 +2,13 @@ import itertools
 
 from brainscore.submission.ml_pool import ModelLayers
 
+def resnet_pt_layers(units):
+    return (['relu', 'maxpool'] +
+            [f"layer1.{i}" for i in range(units[0])] +
+            [f"layer2.{i}" for i in range(units[1])] +
+            [f"layer3.{i}" for i in range(units[2])] +
+            [f"layer4.{i}" for i in range(units[3])] +
+            ["avgpool"])
 
 def resnet50_layers(bottleneck_version):
     return resnet_layers(bottleneck_version=bottleneck_version, units=[3, 4, 6, 3])
@@ -72,6 +79,14 @@ layers = {
             'classifier.2', 'classifier.5'],  # fc-[relu]{6,7,8}
     'vgg-16': [f'block{i + 1}_pool' for i in range(5)] + ['fc1', 'fc2'],
     'vgg-19': [f'block{i + 1}_pool' for i in range(5)] + ['fc1', 'fc2'],
+    'vgg-11-pt': [f'features.{i}' for i in [1,2,4,5,7,9,10,12,14,15,17,19,20]] + ['classifier.1', 'classifier.4'],
+    'vgg-11-bn-pt': [f'features.{i}' for i in [2,3,6,7,10,13,14,17,20,21,24,27,28]] + ['classifier.1', 'classifier.4'],
+    'vgg-13-pt': [f'features.{i}' for i in [1,3,4,6,8,9,11,13,14,16,18,19,21,23,24]] + ['classifier.1', 'classifier.4'],
+    'vgg-13-bn-pt': [f'features.{i}' for i in [2,5,6,9,12,13,16,19,20,23,26,27,30,33,34]] + ['classifier.1', 'classifier.4'],
+    'vgg-16-pt': [f'features.{i}' for i in [1,3,4,6,8,9,11,13,15,16,18,20,21,23,25,27,29,30]] + ['classifier.1', 'classifier.4'],
+    'vgg-16-bn-pt': [f'features.{i}' for i in [2,5,6,9,12,13,16,19,22,23,26,29,32,33,36,39,42,43]] + ['classifier.1', 'classifier.4'],
+    'vgg-19-pt': [f'features.{i}' for i in [1,3,4,6,8,9,11,13,15,17,18,20,22,24,26,27,29,31,33,35,36]] + ['classifier.1', 'classifier.4'],
+    'vgg-19-bn-pt': [f'features.{i}' for i in [2,5,6,9,12,13,16,19,22,25,26,29,32,35,38,39,42,45,48,51,52]] + ['classifier.1', 'classifier.4'],
     'squeezenet1_0':
         ['features.' + layer for layer in
          # max pool + fire outputs (ignoring pools)
@@ -116,61 +131,75 @@ layers = {
         [f'block13_sepconv{i + 1}_act' for i in range(2)] +
         [f'block14_sepconv{i + 1}_act' for i in range(2)] +
         ['avg_pool'],
-    'resnet-18':
-        ['conv1'] +
-        ['layer1.0.relu', 'layer1.1.relu'] +
-        ['layer2.0.relu', 'layer2.0.downsample.0', 'layer2.1.relu'] +
-        ['layer3.0.relu', 'layer3.0.downsample.0', 'layer3.1.relu'] +
-        ['layer4.0.relu', 'layer4.0.downsample.0', 'layer4.1.relu'] +
-        ['avgpool'],
-    'resnet-34':
-        ['conv1'] +
-        ['layer1.0.conv2', 'layer1.1.conv2', 'layer1.2.conv2'] +
-        ['layer2.0.downsample.0', 'layer2.1.conv2', 'layer2.2.conv2', 'layer2.3.conv2'] +
-        ['layer3.0.downsample.0', 'layer3.1.conv2', 'layer3.2.conv2', 'layer3.3.conv2',
-         'layer3.4.conv2', 'layer3.5.conv2'] +
-        ['layer4.0.downsample.0', 'layer4.1.conv2', 'layer4.2.conv2'] +
-        ['avgpool'],
-    'resnet-50':
-        ['conv1'] +
-        ['layer1.0.conv3', 'layer1.1.conv3', 'layer1.2.conv3'] +
-        ['layer2.0.downsample.0', 'layer2.1.conv3', 'layer2.2.conv3', 'layer2.3.conv3'] +
-        ['layer3.0.downsample.0', 'layer3.1.conv3', 'layer3.2.conv3', 'layer3.3.conv3',
-         'layer3.4.conv3', 'layer3.5.conv3'] +
-        ['layer4.0.downsample.0', 'layer4.1.conv3', 'layer4.2.conv3'] +
-        ['avgpool'],
+    'resnet-18-pt': resnet_pt_layers([2,2,2,2]),
+    'resnet-34-pt': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-pt': resnet_pt_layers([3,4,6,3]),
+    'wide-resnet-50-pt': resnet_pt_layers([3,4,6,3]),
+    'resnet-101-pt': resnet_pt_layers([3,4,23,3]),
+    'wide-resnet-101-pt': resnet_pt_layers([3,4,23,3]),
+    'resnet-152-pt': resnet_pt_layers([3,8,36,3]),
+    'resnext-50-32x4d-pt': resnet_pt_layers([3,4,6,3]),
+    'resnext-101-32x8d-pt': resnet_pt_layers([3,4,23,3]),
+    'resnet-50-robust-l2-3': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-robust-linf-4': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-robust-linf-8': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-GNTsig0.5': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-ANT3x3_SIN': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-SIN': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-SIN_IN': resnet_pt_layers([3,4,6,3]),
+    'resnet-50-SIN_IN_IN': resnet_pt_layers([3,4,6,3]),
     'voneresnet-50':
         ['vone_block'] +
-        ['model.layer1.0.conv3', 'model.layer1.1.conv3', 'model.layer1.2.conv3'] +
-        ['model.layer2.0.downsample.0', 'model.layer2.1.conv3', 'model.layer2.2.conv3', 'model.layer2.3.conv3'] +
-        ['model.layer3.0.downsample.0', 'model.layer3.1.conv3', 'model.layer3.2.conv3', 'model.layer3.3.conv3',
-         'model.layer3.4.conv3', 'model.layer3.5.conv3'] +
-        ['model.layer4.0.downsample.0', 'model.layer4.1.conv3', 'model.layer4.2.conv3'] +
+        ['model.layer1.0', 'model.layer1.1', 'model.layer1.2'] +
+        ['model.layer2.0', 'model.layer2.1', 'model.layer2.2', 'model.layer2.3'] +
+        ['model.layer3.0', 'model.layer3.1', 'model.layer3.2', 'model.layer3.3',
+         'model.layer3.4', 'model.layer3.5'] +
+        ['model.layer4.0', 'model.layer4.1', 'model.layer4.2'] +
         ['model.avgpool'],
     'voneresnet-50-robust':
         ['vone_block'] +
-        ['model.layer1.0.conv3', 'model.layer1.1.conv3', 'model.layer1.2.conv3'] +
-        ['model.layer2.0.downsample.0', 'model.layer2.1.conv3', 'model.layer2.2.conv3', 'model.layer2.3.conv3'] +
-        ['model.layer3.0.downsample.0', 'model.layer3.1.conv3', 'model.layer3.2.conv3', 'model.layer3.3.conv3',
-         'model.layer3.4.conv3', 'model.layer3.5.conv3'] +
-        ['model.layer4.0.downsample.0', 'model.layer4.1.conv3', 'model.layer4.2.conv3'] +
+        ['model.layer1.0', 'model.layer1.1', 'model.layer1.2'] +
+        ['model.layer2.0', 'model.layer2.1', 'model.layer2.2', 'model.layer2.3'] +
+        ['model.layer3.0', 'model.layer3.1', 'model.layer3.2', 'model.layer3.3',
+         'model.layer3.4', 'model.layer3.5'] +
+        ['model.layer4.0', 'model.layer4.1', 'model.layer4.2'] +
         ['model.avgpool'],
-    'resnet-50-robust':
-        ['conv1'] +
-        ['layer1.0.conv3', 'layer1.1.conv3', 'layer1.2.conv3'] +
-        ['layer2.0.downsample.0', 'layer2.1.conv3', 'layer2.2.conv3', 'layer2.3.conv3'] +
-        ['layer3.0.downsample.0', 'layer3.1.conv3', 'layer3.2.conv3', 'layer3.3.conv3',
-         'layer3.4.conv3', 'layer3.5.conv3'] +
-        ['layer4.0.downsample.0', 'layer4.1.conv3', 'layer4.2.conv3'] +
-        ['avgpool'],
-    'resnet-50-pytorch':
-        ['conv1'] +
-        ['layer1.0.conv3', 'layer1.1.conv3', 'layer1.2.conv3'] +
-        ['layer2.0.downsample.0', 'layer2.1.conv3', 'layer2.2.conv3', 'layer2.3.conv3'] +
-        ['layer3.0.downsample.0', 'layer3.1.conv3', 'layer3.2.conv3', 'layer3.3.conv3',
-         'layer3.4.conv3', 'layer3.5.conv3'] +
-        ['layer4.0.downsample.0', 'layer4.1.conv3', 'layer4.2.conv3'] +
-        ['avgpool'],
+    'densenet-121-pt':
+        ['features.relu0', 'features.pool0'] +
+        [f'features.denseblock1.denselayer{i + 1}.relu1' for i in range(6)] + 
+        ['features.transition1.relu'] +
+        [f'features.denseblock2.denselayer{i + 1}.relu1' for i in range(12)] + 
+        ['features.transition2.relu'] +
+        [f'features.denseblock3.denselayer{i + 1}.relu1' for i in range(24)] + 
+        ['features.transition3.relu'] +
+        [f'features.denseblock4.denselayer{i + 1}.relu1' for i in range(16)],
+    'densenet-169-pt':
+        ['features.relu0', 'features.pool0'] +
+        [f'features.denseblock1.denselayer{i + 1}.relu1' for i in range(6)] + 
+        ['features.transition1.relu'] +
+        [f'features.denseblock2.denselayer{i + 1}.relu1' for i in range(12)] + 
+        ['features.transition2.relu'] +
+        [f'features.denseblock3.denselayer{i + 1}.relu1' for i in range(32)] + 
+        ['features.transition3.relu'] +
+        [f'features.denseblock4.denselayer{i + 1}.relu1' for i in range(32)],
+    'densenet-201-pt':
+        ['features.relu0', 'features.pool0'] +
+        [f'features.denseblock1.denselayer{i + 1}.relu1' for i in range(6)] + 
+        ['features.transition1.relu'] +
+        [f'features.denseblock2.denselayer{i + 1}.relu1' for i in range(12)] + 
+        ['features.transition2.relu'] +
+        [f'features.denseblock3.denselayer{i + 1}.relu1' for i in range(48)] + 
+        ['features.transition3.relu'] +
+        [f'features.denseblock4.denselayer{i + 1}.relu1' for i in range(32)],
+    'densenet-161-pt':
+        ['features.relu0', 'features.pool0'] +
+        [f'features.denseblock1.denselayer{i + 1}.relu2' for i in range(6)] + 
+        ['features.transition1.relu'] +
+        [f'features.denseblock2.denselayer{i + 1}.relu2' for i in range(12)] + 
+        ['features.transition2.relu'] +
+        [f'features.denseblock3.denselayer{i + 1}.relu2' for i in range(36)] + 
+        ['features.transition3.relu'] +
+        [f'features.denseblock4.denselayer{i + 1}.relu2' for i in range(24)],
     # Slim
     'inception_v1':
         ['MaxPool_2a_3x3'] +
@@ -251,13 +280,6 @@ layers = {
 
 model_layers = ModelLayers(layers)
 model_layers['vggface'] = model_layers['vgg-16']
-for sin_model in ['resnet50-SIN', 'resnet50-SIN_IN', 'resnet50-SIN_IN_IN']:
-    model_layers[sin_model] = \
-        ['conv1'] + \
-        [f'layer{seq}.{bottleneck}.relu'
-         for seq, bottlenecks in enumerate([3, 4, 6, 3], start=1)
-         for bottleneck in range(bottlenecks)] + \
-        ['avgpool']
 
 for version, multiplier, image_size in [
     # v1
