addcdb08d069b247724be018a0a67832f2692d42
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 10:27:04 +0000

    Adding tarfile member sanitization to extractall()

--- candidate_models/base_models/unsupervised_vvs/__init__.py
+++ candidate_models/base_models/unsupervised_vvs/__init__.py
@@ -101,7 +101,26 @@ class ModelBuilder:
                 tar_file.write(r.content)
             if identifier not in self.PT_MODELS:
                 with tarfile.open(tar_path) as tar:
-                    tar.extractall(path=weightsdir_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=weightsdir_path)
                 os.remove(tar_path)
         if identifier not in self.PT_MODELS:
             return os.path.join(weightsdir_path, os.path.basename(aws_path)[:-4])
